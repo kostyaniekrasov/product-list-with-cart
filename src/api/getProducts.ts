204166c3edcf81aadbcbdaaf204181bf86478ac7
Kostya Niekrasov <kostyanekrasov1501@gmail.com>
2025-03-10 09:29:01 +0200

    Product list with svg animations

--- src/api/getProducts.ts
+++ src/api/getProducts.ts
@@ -2,7 +2,7 @@ import { ProductType } from '@/types';
 
 const getProducts = async () => {
   try {
-    const response = await fetch('/products/data.json');
+    const response = await fetch('products/data.json');
 
     if (!response.ok) {
       throw new Error('Network response was not ok');
